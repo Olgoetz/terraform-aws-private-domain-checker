a59fad3f835c080d7f751b20c7fcf345dc1f25c2
oliver.goetz@axa.com <oliver.goetz@axa.com>
2022-06-26 15:59:47 +0200

    [OlGo] bug fixed

--- files.tf
+++
@@ -1,9 +0,0 @@
-resource "local_file" "502" {
-  content  = var.html_content_502
-  filename = "${path.module}/sources/502.html"
-}
-
-resource "local_file" "503" {
-  content  = var.html_content_503
-  filename = "${path.module}/sources/503.html"
-}
